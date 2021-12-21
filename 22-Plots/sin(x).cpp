5c3b747a1ecdb31844a9e460e582e6ec6d0aacb1
Vivikills <90639876+Vivikills@users.noreply.github.com>
2021-12-21 04:32:23 +0300

    Add files via upload

---
+++ 22-Plots/sin(x).cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<cmath>
+#include<fstream>
+
+using namespace std;
+
+int main()
+{
+	double cord_x; 
+	double cord_y;
+    ofstream fout;
+    string path = "Data5.txt";
+	fout.open(path, ofstream::app);
+    if (!fout.is_open())
+	{
+		cout << "Error of openning";
+	}
+	else
+	{
+		for (int i = -100; i <= 100; i++)
+	    {
+			cord_x = i;
+		    cord_y = cord_x*sin(cord_x);
+			fout << cord_x << "   " << cord_y;
+			fout << "\n";
+			cout << cord_x << " ";
+	    }
+	}
+	fout.close();
+
+	int c = 0;
+	cin >> c;
+	return 0;
+}
+        
